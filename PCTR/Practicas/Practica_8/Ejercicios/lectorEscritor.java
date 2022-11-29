20ed7756a1503423522d917e5c0b4ebf29c6ba17
Juanma343 <juanmanuelgron343@gmail.com>
2022-11-29 15:08:23 +0100

    terminado sin que funciones

--- PCTR/Practicas/Practica_8/Ejercicios/lectorEscritor.java
+++ PCTR/Practicas/Practica_8/Ejercicios/lectorEscritor.java
@@ -1,40 +1,39 @@
-public class lectorEscritor extends Thread {
+public class lectorEscritor {
     private static boolean escribiendo = false;
     private static Object lector = new Object();
     private static Object escritor = new Object();
     private static int lectores = 0;
-    private int tipo = 0;
 
-    public lectorEscritor(int _tipo){
-        tipo = _tipo;
-    }
+    public lectorEscritor(){}
 
-    public void run(){
-        switch(tipo){
-            case 1: 
-                synchronized(lector){
-                    if (escribiendo) { try { lector.wait(); } catch (InterruptedException e) {e.printStackTrace();} } 
-                    lectores++;
-                    lector.notifyAll();
-                } break;
-                
-            case 2: 
-                synchronized(lector){
-                    lectores--;
-                    if(lectores == 0){ lector.notifyAll(); }
-                } break;
-            case 3: 
-                synchronized(escritor){
-                    if (lectores != 0 || escribiendo) { try { escritor.wait(); } catch (InterruptedException e) {e.printStackTrace();} }
-                    escribiendo = true;
-                } break;
-            case 4: 
-                synchronized(escritor){
-                    escribiendo = false;
-                    if (lectores == 0) { escritor.notifyAll(); }
-                    else { lector.notifyAll(); }
-                }
-            break;
+    public void iniciaLectura(){
+        synchronized(lector){
+            if (escribiendo) { try { lector.wait(); } catch (InterruptedException e) {e.printStackTrace();} } 
+            lectores++;
+            lector.notifyAll();
         }
     }
+
+    public void acabarLectura(){
+        synchronized(lector){
+            lectores--;
+            if(lectores == 0){ lector.notifyAll(); }
+        }
+    }
+
+    public void iniciaEscritura(){
+        synchronized(escritor){
+            if (lectores != 0 || escribiendo) { try { escritor.wait(); } catch (InterruptedException e) {e.printStackTrace();} }
+            escribiendo = true;
+        }
+    }
+
+    public void acabarEscritura(){
+        synchronized(escritor){
+            escribiendo = false;
+            if (lectores == 0) { escritor.notifyAll(); }
+            else { lector.notifyAll(); }
+        }
+    }
+
 }
--- PCTR/Practicas/Practica_8/Ejercicios/usalectorEscritor.java
+++ PCTR/Practicas/Practica_8/Ejercicios/usalectorEscritor.java
@@ -1,4 +1,42 @@
-public class usalectorEscritor {
+public class usalectorEscritor extends Thread {
     private static recurso rec = new recurso();
+    private static lectorEscritor lec = new lectorEscritor();
+    private static long data = 0;
+    private int tipo = 0;
+
+    public usalectorEscritor(int i){
+        tipo = i;
+    }
+
+    public void run(){
+        switch(tipo){
+            case 2: 
+                for(long i = 0; i < 1000000; i++){
+                    lec.iniciaLectura();
+                    data = rec.observar();
+                    lec.acabarLectura();
+                }
+            break;
+            case 1: 
+                for(long i = 0; i < 1000000; i++){
+                    lec.iniciaEscritura();
+                    rec.inc();
+                    lec.acabarEscritura();
+                }
+            break;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] vect = new Thread[4];
+        for (int i = 0; i < 4; i++){
+            vect[i] = new usalectorEscritor( (i % 2) + 1);
+            vect[i].start();
+        }
+        for (int i = 0; i < 4; i++){
+            vect[i].join();
+        }
+        System.out.println(data);
+    }
     
 }
